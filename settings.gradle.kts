36023f2bda011ed9df1a5ce81349440853fcec9e
PrabhakarYdv <prabhakarkr802@gmail.com>
2025-02-09 22:35:33 +0530

    Welcome Screen

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Carrier Guidance App"
+include(":app")
+ 
